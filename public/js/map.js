2b2f82ab8324613f96d89a98de3423aebd0123ec
HansData <arraihan0104@gmail.com>
2023-07-18 08:39:38 +0900

    merapikan tampilan halaman map.html dan menambah dekorasi pada halaman

--- public/js/map.js
+++ public/js/map.js
@@ -1,9 +1,6 @@
 $(window).ready(() => {
 
-    // !##############pencobaan fungsi
-
-
-
+    // ! ########### function menentukan lebar layar untuk menentukan settingna slick
     function getScreen() {
 
         const lebarLayar = $('body').outerWidth(true);
@@ -19,9 +16,14 @@ $(window).ready(() => {
 
     getScreen()
 
+    // ? ########## parallax scroll bg
+    $(window).scroll(() => {
+        let scroll = window.scrollY
+        $('.container-main-map').css('background-position-y', `${scroll / 1.5}px`)
+    });
 
 
-})
+});
 
 
 const data = fetch('https://valorant-api.com/v1/maps')
@@ -40,14 +42,22 @@ const data = fetch('https://valorant-api.com/v1/maps')
             if (elemen.uuid !== 'ee613ee9-28b7-4beb-9666-08db13bb2244') {
 
                 $('#slide-slick').append(`
-                <div class="conten-slick relative bg-slate-700">
-              <img class="lg:w-[100%]" src="${elemen.splash}" alt="" />
+            <div class="conten-slick relative bg-slate-700">
+            <div class="relative border-[5px] border-white">
+                <img class="lg:w-[100%]" src="${elemen.splash}" alt="gambar peta" />
 
-              <div class="denah-map relative lg:absolute lg:bottom-0 lg:right-0 lg:w-48 lg:h-48 p-5 bg-white group lg:hover:w-full lg:hover:h-full lg:hover:bg-white/50 transition-all">
-                <span class="text-sm font-semibold absolute left-0 top-0 rotate-90 group-hover:lg:text-lg text-red-500">${getIndex()}</span>
+                <div class=" top-0 w-[10vw] h-[15vw] before:w-4/5 before:h-1/3 after:h-4/5 after:w-1/3 dekor-slide-map"></div>
+                <div class=" bottom-0 w-[25vw] h-[8vw] before:w-1/3 before:h-4/5 before:bottom-0 after:h-1/4 after:w-4/5 after:bottom-0 dekor-slide-map"></div>
+                <div class="right-0  top-0 w-[25vw] h-[8vw] before:w-1/3 before:h-4/5 before:top-0 before:right-0 after:h-1/4 after:w-4/5 after:top-0 after:right-0 dekor-slide-map"></div>
+                <div class="right-0  bottom-0 w-[10vw] h-[15vw] before:w-4/5 before:h-1/3 before:bottom-0 before:right-0 after:h-4/5 after:w-1/3 after:bottom-0 after:right-0 dekor-slide-map"></div>
+
+            </div>
+
+            <div class="denah-map relative lg:absolute lg:bottom-0 lg:right-0 lg:w-48 lg:h-48  p-5 bg-teal-400 group lg:hover:w-full lg:hover:h-full lg:hover:bg-teal-400/50 transition-all">
+                <span class="z-50 text-sm font-semibold absolute left-0 top-0 rotate-90 group-hover:lg:text-lg text-red-500">${getIndex()}</span>
                 <h3 class="text-base font-semibold mb-2.5 group-hover:lg:text-4xl group-hover:lg:ml-5">${elemen.displayName.toUpperCase()}</h3>
                 <img class="lg:w-[80%] group-hover:lg:w-1/2 bayangan2 border-2 border-stone-600 border-dashed mx-auto" src="${elemen.displayIcon}" alt="" />
-              </div>
+            </div>
             </div>
                 `);
 
@@ -73,11 +83,8 @@ function getSettingSlick() {
         prevArrow: '.arrowPrev',
         nextArrow: '.arrowNext',
         dots: false,
-
-
-
-
     });
+
     $('.slide').slick({
         slidesToShow: 12,
         slidesToScroll: 12,
@@ -91,5 +98,4 @@ function getSettingSlick() {
         touchMove: false
     });
 
-}
-
+};
